53b0c194333ea0ed10b9c5b33dc92e6df4c17399
HyuuunjuKim <gus7wn@gmail.com>
2019-09-30 16:24:56 +0900

    직사각형 별찍기

---
+++ Programmers_12899/Solution.java
@@ -0,0 +1,29 @@
+package Programmers_12899;
+
+public class Solution {
+	public String solution(int n) {
+	    String answer = "";
+	    /*
+	     *n을 -1, -2, 4해서
+	     *나누기 3을 해서 나머지가 0이면
+	     * 3*몫+() = n이므로,,,
+	     * "몫"+"()"
+	     */
+	    int a=0;
+	    
+	    if((n-1)%3 == 0) {
+	    	a = a + (n-1)/3;
+	    	answer = Integer.toString(a)+"1";
+	    }
+	    else if ((n-2) % 3 == 0) {
+	    	a = a + (n-2)/3;
+	    	answer = Integer.toString(a)+"1";
+	    }
+	    else if ((n-4) % 3 == 0) {
+	    	a = a + (n-4)/3;
+	    	answer = Integer.toString(a)+"1";
+	    }
+	    return answer;
+	}
+
+}
---
+++ Programmers_12899/test.java
@@ -0,0 +1,13 @@
+package Programmers_12899;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl = new Solution();
+		int n= 10;
+		
+		System.out.println(sl.solution(n));
+	}
+
+}
