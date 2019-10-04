f7b5b871d9469a08ff6cccb85f347d8d1baa3100
HyuuunjuKim <gus7wn@gmail.com>
2019-10-05 01:52:56 +0900

    짝수와 홀수

---
+++ Programmers_12937/Solution.java
@@ -0,0 +1,15 @@
+package Programmers_12937;
+
+public class Solution {
+	public String solution(int num) {
+		String answer="";
+		if(num % 2 == 0 ) {
+			answer = "Even";
+		}
+		else {
+			answer = "Odd";
+		}
+		return answer;
+	}
+
+}
---
+++ Programmers_12937/test.java
@@ -0,0 +1,12 @@
+package Programmers_12937;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl = new Solution();
+		int num = 4;
+		System.out.println(sl.solution(num));
+	}
+
+}
