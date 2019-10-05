a476661658adfca71357939677bfec8003fac76d
HyuuunjuKim <gus7wn@gmail.com>
2019-10-05 17:58:59 +0900

    평균 구하기

---
+++ Programmers_12944/test.java
@@ -0,0 +1,12 @@
+package Programmers_12944;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl = new Solution();
+		int[] arr = {1,2,3,4};
+		System.out.println(sl.solution(arr));
+	}
+
+}
