89ce7cbb481b1d43528c7f8624bed899173bfc69
HyuuunjuKim <gus7wn@gmail.com>
2019-10-06 00:31:12 +0900

    피보나치 수

---
+++ Programmers_12945/test.java
@@ -0,0 +1,13 @@
+package Programmers_12945;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl  = new Solution();
+		int n = 300;
+		System.out.println(sl.solution(n));
+
+	}
+
+}
