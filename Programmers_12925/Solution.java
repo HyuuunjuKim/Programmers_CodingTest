0dffba820ecfb3fe84420e798316610a5a73d2f8
HyuuunjuKim <gus7wn@gmail.com>
2019-10-05 18:03:07 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers_12925/Solution.java
@@ -0,0 +1,10 @@
+package Programmers_12925;
+
+public class Solution {
+	public int solution(String s) {
+		int answer = 0;
+		answer = Integer.parseInt(s);
+		return answer;
+	}
+
+}
---
+++ Programmers_12925/test.java
@@ -0,0 +1,13 @@
+package Programmers_12925;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl = new Solution();
+		String s = "1234";
+		System.out.println(sl.solution(s));
+
+	}
+
+}
