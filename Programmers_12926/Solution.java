af5251364501bc82cdd434f7fd82043c9e4fae9e
HyuuunjuKim <gus7wn@gmail.com>
2020-08-02 12:43:47 +0900

    시저 암호

--- Programmers_12926/Solution.java
+++ Programmers_12926/Solution.java
@@ -1,30 +1,44 @@
 package Programmers_12926;
+
 import java.util.*;
 
 public class Solution {
 	public String solution(String s, int n) {
 		String answer = "";
 		/*
-		 * 문자 첫번째를 아스키 코드로 바꿈
-		 * 아스키코드 + n
-		 * 이거를 시작으로 s.length만큼 문자열 만들기
-		 * 다시 char로 변경
+		 * 문자 한 글자마다 아스키코드로 바꿈 아스키코드 + n 이거를 시작으로 s.length만큼 문자열 만들기 다시 char로 변경
 		 */
-		
-		int[] array_string = new int[s.length()]; //input S를 스트링 배열에 담은
-		for(int i=0 ; i < s.length(); i++) {
-			int ascii = (int)s.charAt(i); //문자들을 아스키 코드로 바꿈
-			array_string[i] = ascii+n; //아스키코드 + n한 걸 배열에 담음
+
+		char[] s_arr = new char[s.length()]; // input S를 스트링 배열에 담은
+		for (int i = 0; i < s.length(); i++) {
+			int ascii = s.charAt(i); // 문자들을 아스키 코드로 바꿈
+
+			int after_ascii = ascii + n;
+			if(ascii == 32) {
+				after_ascii = 32;
+			}
+			if (ascii <= 90 && after_ascii > 90) {
+				after_ascii -= 26;
+			}
+			if (ascii >= 97 && after_ascii > 122) {
+				after_ascii -= 26;
+			}
+
+			s_arr[i] = (char) after_ascii; // 아스키코드 + n한 걸 배열에 담음
 		}
-		
-		char[] answer_ascii = new char[s.length()]; 
-		for(int i=0 ; i< s.length() ; i++) {
-			answer_ascii[i] = (char)array_string[i];
-		}
-		answer = new String(answer_ascii);
-		
-		
+
+		answer = new String(s_arr);
+
 		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sl = new Solution();
+		String s = "a B z";
+		int n = 1;
+		System.out.println(sl.solution(s, n));
+
 	}
 
 }
